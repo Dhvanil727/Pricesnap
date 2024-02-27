463cd9f1d70dbd2739fbbf9dd3e6ebd186414098
Dhvanil727 <104253028+Dhvanil727@users.noreply.github.com>
2024-02-27 17:30:58 +0530

    Add files via upload

---
+++ client2/src/Products/Phones_List.js
@@ -0,0 +1,39 @@
+// Products_List.js
+const phones = [
+    {
+      id: 1,
+      name: "Product 1",
+      description: "Description for Product 1",
+      imageUrl: '/images/oneplus11r.jpg',
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      description: "Description for Product 2",
+      imageUrl: "/images/iphone15promax.jpg",
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      description: "Description for Product 3",
+      imageUrl: "/images/samsung.jpg",
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      description: "Description for Product 4",
+      imageUrl: "/images/oneplus11r.jpg",
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      description: "Description for Product 5",
+      imageUrl: "/images/oneplus11r.jpg",
+    }
+    // Add more products as needed
+  ];
+  
+  
+  
+  export default phones  ;
+  
---
+++ client2/src/Products/Watches_list.js
@@ -0,0 +1,34 @@
+const watches = [
+    {
+      id: 1,
+      name: "Product 1",
+      description: "Description for Product 1",
+      imageUrl: '/images/titan1.jpg',
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      description: "Description for Product 2",
+      imageUrl: "/images/titan2.jpg",
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      description: "Description for Product 3",
+      imageUrl: "/images/titan3.jpg",
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      description: "Description for Product 4",
+      imageUrl: "/images/titan1.jpg",
+    },
+    {
+      id: 5,
+      name: "Product 5",
+      description: "Description for Product 5",
+      imageUrl: "/images/titan3.jpg",
+    }
+    // Add more products as needed
+  ];
+  export default watches;
